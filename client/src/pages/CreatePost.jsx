647f534f31089bf3d1496e07e11eb2ec664b9261
Menardo Madiumba <menardotera@gmail.com>
2026-07-24 10:20:26 +0100

    Integrate api upload image for cloudinary et receve data, response

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,12 +1,78 @@
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
+import { useState } from "react";
 import ReactQuill from "react-quill-new";
 import "react-quill-new/dist/quill.snow.css";
 
 const CreatePost = () => {
+
+  const [file, setFile] = useState(null)
+  const [imageUrl, setImageUrl] = useState("");
+  const [imagePublicId, setImagePublicId] = useState("");
+
+  const [uploading, setUploading] = useState(false);
+  const [uploadError, setUploadError] = useState(null);
+  const [uploadSuccess, setUploadSuccess] = useState(null);
+
+  const handlerUploadImage = async () => {
+
+    if(!file){
+        setUploadError("Veuillez sélectionner une image.")
+        return 
+    }
+
+    try {
+        
+        setUploading(true)
+        setUploadError(null)
+        setUploadSuccess(null)
+
+        const formData = new FormData()
+
+        formData.append("image", file)
+        formData.append("folder", "posts")  // dossier Cloudinary
+
+        // Sauvegarde au cloudinary
+        const res = await fetch("/api/upload", {
+            method: "POST",
+            body: formData
+        })
+
+        const data = await res.json()
+
+        setUploading(false);
+
+        if(!res.ok || !data.success){
+            setUploadError(data.message);
+            return;
+        }
+
+        setImageUrl(data.url)
+        setImagePublicId(data.public_id)
+        setUploadSuccess("Image chargé avec succes")
+
+        console.log(data)
+
+    } catch (error) {
+        setUploadError(error.message);
+        setUploadSuccess(null)
+    }
+
+  }
+
   return (
     <div className="p-3 max-w-3xl w-full mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Create Post</h1>
       <form action="" className="flex flex-col gap-4">
+            {uploadError && (
+                <Alert color="failure">
+                    {uploadError}
+                </Alert>
+            )}
+            {uploadSuccess && (
+                <Alert color="success">
+                    {uploadSuccess}
+                </Alert>
+            )}
             <div className="flex flex-col gap-4 sm:flex-row justify-between">
                 <TextInput 
                     type="text"
@@ -28,14 +94,17 @@ const CreatePost = () => {
                 <FileInput
                     type="file"
                     accept="image/*"
+                    onChange={(e) => setFile(e.target.files[0])}
                 />
                 <Button
                     type="button"
                     size="sm"
                     outline
                     className="cursor-pointer"
+                    onClick={handlerUploadImage}
+                    disabled={uploading}
                 >
-                    Upload image
+                    {uploading ? "Chargement..." : "Upload image"}
                 </Button>
             </div>
             <ReactQuill 
