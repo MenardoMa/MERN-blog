8d2c9d29053791033f40b499dac6300074775291
Menardo Madiumba <menardotera@gmail.com>
2026-07-24 17:49:03 +0100

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import Router from "express"
 import { verifyToken } from "../middleware/verifyToken.middleware.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 
 const router = Router()
 
 router.post('/create', verifyToken, create)
+router.get('/getPosts', getPosts)
 
 export default router
