46fdb5adfb840cd59700436136c24e44c51eb68f
Menardo Madiumba <menardotera@gmail.com>
2026-08-07 09:46:27 +0100

    create about content

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,31 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">About Mern' Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Quas ea iusto commodi ipsam perferendis. Nam repellendus 
+              culpa consequatur! Impedit sit exercitationem vero adipisci 
+              reprehenderit reiciendis officiis incidunt, asperiores nisi natus.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Quas ea iusto commodi ipsam perferendis. Nam repellendus 
+              culpa consequatur! Impedit sit exercitationem vero adipisci 
+              reprehenderit reiciendis officiis incidunt, asperiores nisi natus.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Quas ea iusto commodi ipsam perferendis. Nam repellendus 
+              culpa consequatur! Impedit sit exercitationem vero adipisci 
+              reprehenderit reiciendis officiis incidunt, asperiores nisi natus.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
