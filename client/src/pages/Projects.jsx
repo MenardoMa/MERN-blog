8f52f026b7b295013a1cfac88c9ccd02eb71b27a
Menardo Madiumba <menardotera@gmail.com>
2026-08-07 09:51:55 +0100

    Complete Projets page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
+import CallToAction from "../components/CallToAction"
+
 const Projects = () => {
   return (
-    <div>
-       <h1>Projects</h1>
+    <div className="min-h-screen max-w-2xl mx-auto flex flex-col items-center justify-center gap-6 p-3 text-center">
+       <h1 className="text-3xl font-semibold">Projet</h1>
+       <p className="text-md text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Vitae amet ducimus impedit? Iusto, repellat tempore dolores
+       </p>
+       <CallToAction />
     </div>
   )
 }
